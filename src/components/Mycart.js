ac6a2899b7acc16778e16122b98ee0a6f3992484
Piyush0049 <piyushjoshi81204@gmail.com>
2024-05-22 16:50:27 +0530

    commit

--- src/components/Mycart.js
+++ src/components/Mycart.js
@@ -6,7 +6,6 @@ import { addtocart } from './actions/cartactions';
 import { removefromcart } from './actions/cartactions';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
 
 const MyCart = () => {
     const navigate = useNavigate();
