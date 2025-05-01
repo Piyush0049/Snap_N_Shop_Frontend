14ee544cc8c942e9f01d3c312cf3c0f4db0b120f
piyush0049 <piyushjoshi81204@gmail.com>
2025-05-02 05:02:28 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate
+  Navigate,
 } from "react-router-dom";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import Prodpage from "./components/Prodpage";
-import Allproducts from "./components/Allproducts"
+import Allproducts from "./components/Allproducts";
 import Searchbar from "./components/Searchbar";
 import LoginPage from "./components/Login";
 import Account from "./components/Account";
@@ -19,7 +19,7 @@ import React from "react";
 import Forgotpassword from "./components/Forgotpassword";
 import Mycart from "./components/Mycart";
 import Shippingpage from "./components/Shippingpage";
-import Payment from "./components/Payment"
+import Payment from "./components/Payment";
 import ConfirmOrder from "./components/ConfirmOrder";
 import axios from "axios";
 import { Elements } from "@stripe/react-stripe-js";
@@ -29,7 +29,7 @@ import Myorders from "./components/Myorders";
 import Dashboard from "./components/Dashboard";
 function App() {
   const { isAuthenticated } = useSelector((state) => state.userdetails);
-  const ud = useSelector((state) => state.userdetails)
+  const ud = useSelector((state) => state.userdetails);
   useEffect(() => {
     if (Object.keys(ud).length === 1) {
       localStorage.setItem("status", "none");
@@ -44,7 +44,7 @@ function App() {
 
   if (localStorage.getItem("status") === "none") {
     if (window.innerWidth < 1350) {
-      localStorage.setItem("width", window.innerWidth)
+      localStorage.setItem("width", window.innerWidth);
     }
   }
 
@@ -52,15 +52,18 @@ function App() {
     localStorage.removeItem("status");
   }
 
-  console.log(window.innerWidth)
+  console.log(window.innerWidth);
   const dispatch = useDispatch();
   const [Stripeapikey, setstripeapikey] = useState("");
 
   useEffect(() => {
     const getsapikey = async () => {
-      const { data } = await axios.get("https://ecommerce-backend-ochre-two.vercel.app/api/v1/stripeapikey", { withCredentials: true });
+      const { data } = await axios.get(
+        "https://ecommerce-backend-ochre-two.vercel.app/api/v1/stripeapikey",
+        { withCredentials: true }
+      );
       setstripeapikey(data.stripeapikey);
-    }
+    };
     getsapikey();
   }, [dispatch, Stripeapikey]);
 
@@ -69,22 +72,26 @@ function App() {
       <Router>
         <Headers />
         <Routes>
+          {/* Common Routes */}
+          <Route path="/product/:id" element={<Prodpage />} />
+          <Route path="/products" element={<Allproducts />} />
+          <Route path="/products/:keyword" element={<Allproducts />} />
+          <Route path="/search" element={<Searchbar />} />
+          <Route path="/password/forgot" element={<Forgotpassword />} />
+          <Route path="/auth/password/reset/:id" element={<Getnewpassword />} />
+
           {isAuthenticated ? (
             <>
               <Route path="/" element={<Navigate to="/Home" />} />
               <Route path="/Home" element={<Home />} />
               <Route path="/login" element={<LoginPage />} />
-              <Route path="/product/:id" element={<Prodpage />} />
-              <Route path="/products" element={<Allproducts />} />
-              <Route path="/products/:keyword" element={<Allproducts />} />
-              <Route path="/search" element={<Searchbar />} />
               <Route path="/account" element={<Account />} />
-              <Route path="/password/forgot" element={<Forgotpassword />} />
               <Route path="/mycart" element={<Mycart />} />
-              <Route path="/auth/password/reset/:id" element={<Getnewpassword />} />
               <Route path="/shipping" element={<Shippingpage />} />
               <Route path="/confirmorder" element={<ConfirmOrder />} />
-              <Route path="/dashboard" element={<Dashboard />}></Route>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/success" element={<Success />} />
+              <Route path="/myorders" element={<Myorders />} />
               <Route
                 path="/payment"
                 element={
@@ -95,21 +102,13 @@ function App() {
                   )
                 }
               />
-              <Route path="/success" element={<Success />} />
-              <Route path="/myorders" element={<Myorders />} />
             </>
           ) : (
             <>
               <Route path="/" element={<Navigate to="/login" />} />
               <Route path="/Home" element={<LoginPage />} />
               <Route path="/login" element={<LoginPage />} />
-              <Route path="/product/:id" element={<Prodpage />} />
-              <Route path="/products" element={<Allproducts />} />
-              <Route path="/products/:keyword" element={<Allproducts />} />
-              <Route path="/search" element={<Searchbar />} />
               <Route path="/account" element={<LoginPage />} />
-              <Route path="/password/forgot" element={<Forgotpassword />} />
-              <Route path="/auth/password/reset/:id" element={<Getnewpassword />} />
               <Route path="/mycart" element={<LoginPage />} />
               <Route path="/shipping" element={<LoginPage />} />
               <Route path="/payment" element={<LoginPage />} />
